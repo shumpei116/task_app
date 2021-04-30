46a45719dc843145a40581a9d3b92015964adfc7
shumpei116 <>
2021-05-01 07:03:48 +0900

    エラー文追加・メモカラムの追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,7 +44,7 @@ class TasksController < ApplicationController
 
   private
     def task_params
-      params.require(:task).permit(:title, :start_date, :end_date, :all_day)
+      params.require(:task).permit(:title, :start_date, :end_date, :all_day, :memo)
     end
 
     def get_tasks
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_29_044437) do
+ActiveRecord::Schema.define(version: 2021_04_30_215457) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_04_29_044437) do
     t.boolean "all_day", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "memo"
   end
 
 end
