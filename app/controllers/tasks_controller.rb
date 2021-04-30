2d060cd7e399c0788d9f86d7093a61bfc81e43ed
shumpei116 <>
2021-04-30 21:58:28 +0900

    エディットビューの作成・bootstrapインストール

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,7 +15,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      flash[:success] = "新規タスクが作成されました"
+      flash[:success] = "新規スケジュールが作成されました"
       redirect_to tasks_path
     else
       render "new"
@@ -27,11 +27,19 @@ class TasksController < ApplicationController
   end
 
   def update
-
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      flash[:success] = "スケジュールが更新されました"
+      redirect_to tasks_path
+    else
+      render "edit"
+    end
   end
 
   def destroy
-
+    User.find(params[:id]).destroy
+    flash[:success] = "スケジュールを削除しました"
+    redirect_to tasks_path
   end
 
   private
